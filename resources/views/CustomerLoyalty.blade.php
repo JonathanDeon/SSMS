8103ed4c4e66e7aef56e812d7af92d0a9f79d6eb
joelshevin <joelshevinrog@gmail.com>
2016-09-01 20:16:24 +0530

    Added loyalty table

--- resources/views/CustomerLoyalty.blade.php
+++ resources/views/CustomerLoyalty.blade.php
@@ -280,124 +280,14 @@
                                 <tr role="row"><th aria-label="Rendering engine: activate to sort column descending" aria-sort="ascending" style="width: 181px;" colspan="1" rowspan="1" aria-controls="example1" tabindex="0" class="sorting_asc">Rendering engine</th><th aria-label="Browser: activate to sort column ascending" style="width: 224px;" colspan="1" rowspan="1" aria-controls="example1" tabindex="0" class="sorting">Browser</th><th aria-label="Platform(s): activate to sort column ascending" style="width: 198px;" colspan="1" rowspan="1" aria-controls="example1" tabindex="0" class="sorting">Platform(s)</th><th aria-label="Engine version: activate to sort column ascending" style="width: 155px;" colspan="1" rowspan="1" aria-controls="example1" tabindex="0" class="sorting">Engine version</th><th aria-label="CSS grade: activate to sort column ascending" style="width: 110px;" colspan="1" rowspan="1" aria-controls="example1" tabindex="0" class="sorting">CSS grade</th></tr>
                                 </thead>
                                 <tbody>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
                                 <tr class="odd" role="row">
                                     <td class="sorting_1">Gecko</td>
                                     <td>Firefox 1.0</td>
                                     <td>Win 98+ / OSX.2+</td>
                                     <td>1.7</td>
                                     <td>A</td>
-                                </tr><tr class="even" role="row">
-                                    <td class="sorting_1">Gecko</td>
-                                    <td>Firefox 1.5</td>
-                                    <td>Win 98+ / OSX.2+</td>
-                                    <td>1.8</td>
-                                    <td>A</td>
-                                </tr><tr class="odd" role="row">
-                                    <td class="sorting_1">Gecko</td>
-                                    <td>Firefox 2.0</td>
-                                    <td>Win 98+ / OSX.2+</td>
-                                    <td>1.8</td>
-                                    <td>A</td>
-                                </tr><tr class="even" role="row">
-                                    <td class="sorting_1">Gecko</td>
-                                    <td>Firefox 3.0</td>
-                                    <td>Win 2k+ / OSX.3+</td>
-                                    <td>1.9</td>
-                                    <td>A</td>
-                                </tr><tr class="odd" role="row">
-                                    <td class="sorting_1">Gecko</td>
-                                    <td>Camino 1.0</td>
-                                    <td>OSX.2+</td>
-                                    <td>1.8</td>
-                                    <td>A</td>
-                                </tr><tr class="even" role="row">
-                                    <td class="sorting_1">Gecko</td>
-                                    <td>Camino 1.5</td>
-                                    <td>OSX.3+</td>
-                                    <td>1.8</td>
-                                    <td>A</td>
-                                </tr><tr class="odd" role="row">
-                                    <td class="sorting_1">Gecko</td>
-                                    <td>Netscape 7.2</td>
-                                    <td>Win 95+ / Mac OS 8.6-9.2</td>
-                                    <td>1.7</td>
-                                    <td>A</td>
-                                </tr><tr class="even" role="row">
-                                    <td class="sorting_1">Gecko</td>
-                                    <td>Netscape Browser 8</td>
-                                    <td>Win 98SE+</td>
-                                    <td>1.7</td>
-                                    <td>A</td>
-                                </tr><tr class="odd" role="row">
-                                    <td class="sorting_1">Gecko</td>
-                                    <td>Netscape Navigator 9</td>
-                                    <td>Win 98+ / OSX.2+</td>
-                                    <td>1.8</td>
-                                    <td>A</td>
-                                </tr><tr class="even" role="row">
-                                    <td class="sorting_1">Gecko</td>
-                                    <td>Mozilla 1.0</td>
-                                    <td>Win 95+ / OSX.1+</td>
-                                    <td>1</td>
-                                    <td>A</td>
-                                </tr></tbody>
+                                </tr>
+                                </tbody>
                                 <tfoot>
                                 <tr><th colspan="1" rowspan="1">Rendering engine</th><th colspan="1" rowspan="1">Browser</th><th colspan="1" rowspan="1">Platform(s)</th><th colspan="1" rowspan="1">Engine version</th><th colspan="1" rowspan="1">CSS grade</th></tr>
                                 </tfoot>
