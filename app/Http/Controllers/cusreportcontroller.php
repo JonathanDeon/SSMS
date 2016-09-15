1b4c868321adc24f255cd094810438adadbf7548
joelshevin <joelshevinrog@gmail.com>
2016-09-15 18:39:35 +0530

    added report

---
+++ app/Http/Controllers/cusreportcontroller.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\customer;
+
+use App\Http\Requests\RegisterCustomerRequest;
+
+use DB;
+
+class cusreportcontroller extends Controller
+{
+
+    public function Customerreport(){
+
+        $cusid= \DB::select("SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA ='ssms' AND TABLE_NAME ='customer';");
+        $customer= \DB::select("select * from customer");
+        $vehicle= \DB::select("select * from vehicle");
+
+
+        return view('customerReport',compact('customer','cusid','vehicle'));
+    }
+
+    public function LoyaltyReport(){
+
+
+        $customers = \DB::select("select name,cus_id from customer");
+        $data= \DB::select("select * from loyalty inner join customer on loyalty.cusid=customer.cus_id");
+
+        return view('loyaltyReport',compact('customers','data'));
+    }
+
+}
